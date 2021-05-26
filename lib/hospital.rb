9612e51789cee84a65e3639db5d07f984aa478f4
sami-p <sami.peterson14@gmail.com>
2021-05-26 10:29:10 -0500

    Add #doctors instance variable / attr_reader

--- lib/hospital.rb
+++ lib/hospital.rb
@@ -1,9 +1,11 @@
 class Hospital
   attr_reader :name,
-              :chief_of_surgery
+              :chief_of_surgery,
+              :doctors
 
   def initialize(name, chief_of_surgery, doctors)
     @name = name
     @chief_of_surgery = chief_of_surgery
+    @doctors = doctors
   end
 end
