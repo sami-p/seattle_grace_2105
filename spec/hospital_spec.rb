b6c30bef4d4ac4816702da107999026b2229e2da
sami-p <sami.peterson14@gmail.com>
2021-05-26 10:27:01 -0500

    Test that seattle_grace has a #chief_of_surgery

--- spec/hospital_spec.rb
+++ spec/hospital_spec.rb
@@ -18,5 +18,13 @@ RSpec.describe Hospital do
 
       expect(seattle_grace.name).to eq("Seattle Grace")
     end
+
+    it 'has a chief of surgery' do
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+      alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+      seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+      expect(seattle_grace.chief_of_surgery).to eq("Richard Webber")
+    end
   end
 end
