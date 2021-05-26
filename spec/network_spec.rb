dc7ab107ac42195ae34b9b00ff5887580674db29
sami-p <sami.peterson14@gmail.com>
2021-05-26 11:49:25 -0500

    Test #doctors_by_hospital

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -57,5 +57,27 @@ RSpec.describe Network do
 
       expect(gsmn.highest_paid_doctor).to eq(miranda)
     end
+
+    it 'can list #doctors_by_hospital' do
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+      alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+      seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+      miranda = Doctor.new({name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University", salary: 150_000})
+      derek = Doctor.new({name: "Derek Sheperd", specialty: "Neurosurgery", education: "University of Pennsylvania", salary: 125_000})
+      grey_sloan = Hospital.new("Grey Sloan Memorial", "Larry Maxwell", [miranda, derek])
+
+      gsmn = Network.new("Greater Seattle Medical Network")
+
+      gsmn.add_hospital(seattle_grace)
+      gsmn.add_hospital(grey_sloan)
+
+      expected = {
+                    seattle_grace => ["Meredith Grey", "Alex Karev"],
+                    grey_sloan => ["Miranda Bailey", "Derek Sheperd"]
+                  }
+                  
+      expect(gsmn.doctors_by_hospital).to eq(expected)
+    end
   end
 end
