ab042537dcfde41d858984669936277f5a254863
sami-p <sami.peterson14@gmail.com>
2021-05-26 10:24:57 -0500

    Test that seattle_grace has a #name

--- spec/hospital_spec.rb
+++ spec/hospital_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe Hospital do
 
       expect(seattle_grace).to be_an_instance_of(Hospital)
     end
+
+    it 'has a name' do
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+      alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+      seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+      expect(seattle_grace.name).to eq("Seattle Grace")
+    end
   end
 end
