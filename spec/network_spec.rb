f4b16ab7c6580ffbb328a3b3714484727d564321
sami-p <sami.peterson14@gmail.com>
2021-05-26 11:59:46 -0500

    Attempt iteration 4

--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -76,8 +76,34 @@ RSpec.describe Network do
                     seattle_grace => ["Meredith Grey", "Alex Karev"],
                     grey_sloan => ["Miranda Bailey", "Derek Sheperd"]
                   }
-                  
+
       expect(gsmn.doctors_by_hospital).to eq(expected)
     end
   end
+  #
+  # describe 'Iteration 4' do
+  #   it 'lists doctors by specialty' do
+  #
+  #     meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+  #     alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+  #     seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+  #
+  #     miranda = Doctor.new({name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University", salary: 150_000})
+  #     derek = Doctor.new({name: "Derek Sheperd", specialty: "Neurosurgery", education: "University of Pennsylvania", salary: 125_000})
+  #     grey_sloan = Hospital.new("Grey Sloan Memorial", "Larry Maxwell", [miranda, derek])
+  #
+  #     gsmn = Network.new("Greater Seattle Medical Network")
+  #
+  #     gsmn.add_hospital(seattle_grace)
+  #     gsmn.add_hospital(grey_sloan)
+  #
+  #     expected = {
+  #                   "General Surgery" => ["Meredith Grey", "Miranda Bailey"],
+  #                   "Pediatric Surgery" => ["Alex Karev"],
+  #                   "Neurosurgery" => ["Derek Sheperd"]
+  #                 }
+  #
+  #     expect(gsmn.doctors_by_specialty).to eq(expected)
+  #   end
+  # end
 end
