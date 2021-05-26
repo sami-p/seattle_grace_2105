ea6ea26a987adc0ee9675f69a7f4aefda56c3b35
sami-p <sami.peterson14@gmail.com>
2021-05-26 10:09:50 -0500

    Test #name instance variable

--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Doctor do
 
       expect(meredith).to be_an_instance_of(Doctor)
     end
+
+    it 'has a name' do
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+
+      expect(meredith.name).to eq("Meredith Grey")
+    end
   end
 end
