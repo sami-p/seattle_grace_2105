b35e1e0c5916413f2f26e0decb0c2e2bf5ec45c0
sami-p <sami.peterson14@gmail.com>
2021-05-26 10:33:17 -0500

    Test that seattle_grace has a #total_salary of 190000

--- spec/hospital_spec.rb
+++ spec/hospital_spec.rb
@@ -35,4 +35,14 @@ RSpec.describe Hospital do
       expect(seattle_grace.doctors).to eq([meredith, alex])
     end
   end
+
+  describe 'salaries' do
+    it 'has a total salary' do
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+      alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+      seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+      expect(seattle_grace.total_salary).to eq(190000)
+    end
+  end
 end
