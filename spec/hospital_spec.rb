15357682556db2ce1a743ba4bb6cfee8f0413b09
sami-p <sami.peterson14@gmail.com>
2021-05-26 10:23:11 -0500

    Test that seattle_grace is an instance of Hospital

---
+++ spec/hospital_spec.rb
@@ -0,0 +1,14 @@
+require './lib/doctor'
+require './lib/hospital'
+
+RSpec.describe Hospital do
+  describe '#initialize' do
+    it 'exists' do
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+      alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+      seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+      expect(seattle_grace).to be_an_instance_of(Hospital)
+    end
+  end
+end
