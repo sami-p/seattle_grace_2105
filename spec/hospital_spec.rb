0fad245c7cde948194f7fe74c4b4d16c1fea60d1
sami-p <sami.peterson14@gmail.com>
2021-05-26 10:40:53 -0500

    Test #known_specialties

--- spec/hospital_spec.rb
+++ spec/hospital_spec.rb
@@ -52,5 +52,13 @@ RSpec.describe Hospital do
 
       expect(seattle_grace.lowest_paid_doctor).to eq("Alex Karev")
     end
+
+    it 'can list known specialties' do
+      meredith = Doctor.new({name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", salary: 100_000})
+      alex = Doctor.new({name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", salary: 90_000})
+      seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [meredith, alex])
+
+      expect(seattle_grace.specialties).to eq(["General Surgery", "Pediatric Surgery"])
+    end
   end
 end
