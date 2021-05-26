3ecd277128b1e6a9ff6be01ef4598bb9957b00e1
sami-p <sami.peterson14@gmail.com>
2021-05-26 10:15:56 -0500

    Add #salary instance variable / attr_reader

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,11 +1,13 @@
 class Doctor
   attr_reader :name,
               :specialty,
-              :education
+              :education,
+              :salary
 
   def initialize(info)
     @name = info[:name]
     @specialty = info[:specialty]
     @education = info[:education]
+    @salary = info[:salary]
   end
 end
